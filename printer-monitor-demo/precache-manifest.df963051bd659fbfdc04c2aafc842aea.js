4d390f07045c3e07904f77a892a78bffe636cf8f
Jerome <josejeromelalunio@gmail.com>
2021-08-21 10:47:15 +0800

    add build for 3d-printer-controller-demo

---
+++ printer-monitor-demo/precache-manifest.df963051bd659fbfdc04c2aafc842aea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd57230c8615b5ad503",
+    "url": "css/app.b71df899.css"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "img/hamburger.86595150.svg"
+  },
+  {
+    "revision": "7c218a8940680dd1d742a85ffc41a2c4",
+    "url": "img/home-image.7c218a89.png"
+  },
+  {
+    "revision": "d511bc56998fb8673227121abb78029e",
+    "url": "index.html"
+  },
+  {
+    "revision": "edd57230c8615b5ad503",
+    "url": "js/app.cf36d45b.js"
+  },
+  {
+    "revision": "97173f54b45243110834",
+    "url": "js/chunk-vendors.9c397ef1.js"
+  },
+  {
+    "revision": "0a0df928ef6e8e95000afaee7aa72806",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
